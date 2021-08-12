9f90576aac4cb847460622fd307ed1df4421445e
Taku <72721832+Taku-at@users.noreply.github.com>
2021-08-13 03:45:10 +0900

    Add files via upload
    Add the main section: about and menu with responsive

---
+++ bootstrap-bakery-master/modernbootsite/scripts.js
@@ -0,0 +1,15 @@
+$(function () {
+    $('.hamburger-menu').on('click', function () {
+        $('.toggle').toggleClass('open');
+        $('.nav-list').toggleClass('open');
+    })
+});
+
+/**
+ * Toggle syntax the Another one 
+ */
+// $(document).ready(function() {
+//     $('.hamburger-menu').click(function () {
+//         $('.toggle').toggleClass('open');
+//     })
+// })
